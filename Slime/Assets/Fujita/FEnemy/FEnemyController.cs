3a95a6df88a9a5ea0db81ee0ee073968df268729
DESKTOP-0L17MQ3\GA2A <h1202516006@hiratagakuen.onmicrosoft.com>
2026-05-21 10:38:57 +0900

    作業途中

---
+++ Slime/Assets/Fujita/FEnemy/FEnemyController.cs
@@ -0,0 +1,84 @@
+using Unity.Hierarchy;
+using UnityEngine;
+
+public class FEnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;          //移動
+    public bool isToRight = false;      //true=右向き false=左向き
+    public float revTime = 0;           //反転時間
+    public LayerMask groundLayer;       //地面レイヤー
+
+    float time = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(isToRight)
+        {
+            transform.localScale = new Vector2(-1, 1);//向きの変更
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(revTime>0)
+        {
+            time += Time.deltaTime;
+            if(time>=revTime)
+            {
+                isToRight = !isToRight;     //フラグを反転
+                time = 0;                   //タイマーを初期化
+                if(isToRight)
+                {
+                    transform.localScale = new Vector2(-1,1);   //向きの変更
+                }
+                else
+                {
+                    transform.localScale = new Vector2(1, 1);   //向きの変更
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //地上判定
+        bool onGround = Physics2D.CircleCast(transform.position,  //発射位置
+                                           0.5f,                  //円の半径          
+                                           Vector2.down,          //発射方向          
+                                           0.5f,                  //発射距離          
+                                           groundLayer);          //検出するレイヤー
+        if(onGround)
+        {
+            //速度を更新する
+            //Rigidbody2Dを取ってくる
+            Rigidbody2D rbody=GetComponent<Rigidbody2D>();
+            if(isToRight)
+            {
+                rbody.linearVelocity = new Vector2(speed, rbody.linearVelocity.y);
+            }
+            else
+            {
+                rbody.linearVelocity = new Vector2(-speed, rbody.linearVelocity.y);
+            }
+        }
+    }
+
+    //接触
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isToRight = !isToRight;     //フラグを反転させる
+        time = 0;                   //タイマーを初期化
+        if(isToRight)
+        {
+            transform.localScale = new Vector2(1, 1);//向きの変更
+        }
+        else
+        {
+            transform.localScale = new Vector2(-1, 1);//向きの変更
+        }
+    }
+
+}
