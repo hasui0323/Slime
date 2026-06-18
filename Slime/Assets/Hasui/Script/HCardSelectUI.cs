2609ae51e63028a539074f0a229aea6d885a3db5
DESKTOP-L1NVSPV\GA2A <h1202519006@hiratagakuen.onmicrosoft.com>
2026-06-18 12:23:18 +0900

    Text追加

--- Slime/Assets/Hasui/Script/HCardSelectUI.cs
+++ Slime/Assets/Hasui/Script/HCardSelectUI.cs
@@ -6,6 +6,10 @@ public class HCardSelectUI : MonoBehaviour
 {
     private HItemSelectManager manager;
 
+    public Text card1Text;
+    public Text card2Text;
+    public Text card3Text;
+
     public string nextSceneName = "GameScene";
 
     int selectIndex = 0;
@@ -40,11 +44,16 @@ public class HCardSelectUI : MonoBehaviour
         card2Image.sprite = GetSprite(manager.randomItems[1]);
         card3Image.sprite = GetSprite(manager.randomItems[2]);
 
+        card1Text.text = GetDescription(manager.randomItems[0]);
+        card2Text.text = GetDescription(manager.randomItems[1]);
+        card3Text.text = GetDescription(manager.randomItems[2]);
+
         UpdateSelect();
 
         Debug.Log(manager.randomItems[0]);
         Debug.Log(manager.randomItems[1]);
         Debug.Log(manager.randomItems[2]);
+
     }
 
     void Update()
@@ -99,14 +108,20 @@ public class HCardSelectUI : MonoBehaviour
         if (selectIndex == 0)
         {
             card1Image.color = selectColor;
+            card1Text.text =
+            GetDescription(manager.randomItems[0]);
         }
         else if (selectIndex == 1)
         {
             card2Image.color = selectColor;
+            card2Text.text =
+           GetDescription(manager.randomItems[1]);
         }
         else if (selectIndex == 2)
         {
             card3Image.color = selectColor;
+            card3Text.text =
+            GetDescription(manager.randomItems[2]);
         }
     }
 
@@ -143,6 +158,42 @@ public class HCardSelectUI : MonoBehaviour
 
             default:
                 return null;
+        }
+    }
+
+    string GetDescription(ItemType item)
+    {
+        switch (item)
+        {
+            case ItemType.Bullet:
+                return "敵を倒せる弾を撃つ\n(1回)";
+
+            case ItemType.Hammer:
+                return "オブジェクトを破壊できる\n(1回)";
+
+            case ItemType.Heart:
+                return "残機が増える\n(1機)";
+
+            case ItemType.Heaven:
+                return "天国と地獄\n（スタート時のみ出現・\n天国の場合永続で足が速くなる\n・地獄の場合即死）";
+
+            case ItemType.Invincibl:
+                return "敵やダメージオブジェクトに対して無敵\n（オブジェクトをすり抜ける・3秒）";
+
+            case ItemType.Jump:
+                return "ハイジャンプができる\n(空中でも使用可能・1回)";
+
+            case ItemType.NoSkill:
+                return "スキルなし";
+
+            case ItemType.Shoes:
+                return "足が速くなる\n(3秒)";
+
+            case ItemType.TimeReset:
+                return "Cキーのダッシュのクールタイムが\n0秒になる(10秒)";
+
+            default:
+                return "";
         }
     }
 
