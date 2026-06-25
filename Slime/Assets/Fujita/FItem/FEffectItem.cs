8318017e26b1b3052fcba5cdf162a4c833ac940a
DESKTOP-0L17MQ3\GA2A <h1202516006@hiratagakuen.onmicrosoft.com>
2026-06-25 11:07:50 +0900

    ヘブン修正

--- Slime/Assets/Fujita/FItem/FEffectItem.cs
+++ Slime/Assets/Fujita/FItem/FEffectItem.cs
@@ -114,22 +114,18 @@ public class FEffectItem : MonoBehaviour
             //Xキーで発射
             if (Input.GetKeyDown(KeyCode.X))
             {
-                //if (Random.value < 0.5f)
-                //{
-                //    // 当たり
-                //    player.speed += 4.0f;
-                //    Debug.Log("当たり！スピードアップ！");
-                //}
-                //else
-                //{
-                //    // ハズレ
-                //    Debug.Log("ハズレ！即死！");
-                //    player.GameOver();
-                //}
-
-                // 当たり
-                player.speed += 4.0f;
-                Debug.Log("当たり！スピードアップ！");
+                if (Random.value < 0.5f)
+                {
+                    // 当たり
+                    player.speed += 4.0f;
+                    Debug.Log("当たり！スピードアップ！");
+                }
+                else
+                {
+                    // ハズレ
+                    Debug.Log("ハズレ！即死！");
+                    player.GameOver();
+                }
 
                 player.HeavenCount--;
 
