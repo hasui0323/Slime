02518be02313509897a9727b75f027b040700584
DESKTOP-L1NVSPV\GA2A <h1202519006@hiratagakuen.onmicrosoft.com>
2026-06-29 09:59:20 +0900

    sad

--- Slime/Assets/Fujita/FPlayer/FPlayerController.cs
+++ Slime/Assets/Fujita/FPlayer/FPlayerController.cs
@@ -336,7 +336,7 @@ public class FPlayerController : MonoBehaviour
 
         //ƒvƒŒƒCƒ„[‚Æ“G‚ÌÕ“Ë‚ğ–³Œø‰»
         Physics2D.IgnoreLayerCollision(
-            LayerMask.NameToLayer("Player"),
+            LayerMask.NameToLayer("Default"),
             LayerMask.NameToLayer("Enemy"),
             true);
 
